f489fa6bbf9130abf9f273df7ff0eed8f39cb72d
petilaire-bell <petilaire-bell@et.esiea.fr>
2025-12-23 18:35:37 -0400

    Updates

--- assets/use-timeout-D_o2iZgr.js
+++
@@ -1,1 +0,0 @@
-import{v as u}from"./QBtn-BDU-HFCU.js";import{Y as i,b as m,g as s}from"./index-7kMOXxUn.js";function f(){let e=null;const o=s();function t(){e!==null&&(clearTimeout(e),e=null)}return i(t),m(t),{removeTimeout:t,registerTimeout(n,r){t(),u(o)===!1&&(e=setTimeout(()=>{e=null,n()},r))}}}export{f as u};
