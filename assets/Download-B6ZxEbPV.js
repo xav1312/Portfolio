6e5cc4095b1a9e668e15fc3f4d13078c50121e83
petilaire-bell <petilaire-bell@et.esiea.fr>
2026-01-28 00:34:57 +0100

    Updates

--- assets/Download-B6ZxEbPV.js
+++
@@ -1,1 +0,0 @@
-import{k as a,l as r,j as t}from"./index-xSAD9jjW.js";var e={},o=a;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,l=o(r()),d=t;u=e.default=(0,l.default)((0,d.jsx)("path",{d:"M5 20h14v-2H5zM19 9h-4V3H9v6H5l7 7z"}),"Download");export{u as d};
