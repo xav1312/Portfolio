804fa74561e9cb939090edbfc7931940a9dbdc82
petilaire-bell <petilaire-bell@et.esiea.fr>
2026-03-18 14:06:22 +0100

    Updates

--- assets/ArrowForward--ZYMMFkw.js
+++
@@ -1,1 +0,0 @@
-import{i as e,j as a}from"./mui-vendor-CyiIOIWV.js";import{r as t}from"./index-CEywrtEh.js";var r={},o=e;Object.defineProperty(r,"__esModule",{value:!0});var u=r.default=void 0,i=o(t()),d=a;u=r.default=(0,i.default)((0,d.jsx)("path",{d:"m12 4-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"}),"ArrowForward");export{u as d};
